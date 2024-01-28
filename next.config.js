3fcf6c641f6902101e6bab11afd34607a07253f4
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2024-01-28 20:59:41 +0100

    Fix deploy 3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    esmExternals: "loose",
-  },
   compiler: {
     styledComponents: true,
   },
